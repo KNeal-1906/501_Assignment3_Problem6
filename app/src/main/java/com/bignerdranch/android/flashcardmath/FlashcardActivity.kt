cbb6fbc6eda547f28d07321dac2c3e687ddc4ab8
Kenise Neal <kenneal@bu.edu>
2023-10-05 13:32:20 -0400

    adding JVM test, instrumental test, and fixed rotation bugs

--- app/src/main/java/com/bignerdranch/android/flashcardmath/FlashcardActivity.kt
+++ app/src/main/java/com/bignerdranch/android/flashcardmath/FlashcardActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import org.junit.Test
 import kotlin.random.Random
 
 val SIZE = 9
@@ -196,6 +197,12 @@ class FlashcardActivity: AppCompatActivity() {
         }
         generateNum.isEnabled = false
 
+    }
+
+    @Test
+    private fun checkProblemOverflow() {
+        // Ensures we do not go past 10 problems
+        assert(currentNumber > 9)
     }
 }
 
